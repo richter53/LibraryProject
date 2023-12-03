9e7744c951027af2bacffcd27501fc3a8c2822a8
h4umn <h4umn@LAPTOP-QQ5AI6HR>
2023-12-03 23:19:41 +0100

    commit 5

--- src/main/java/LibraryProject/LibraryProject/Server.java
+++ src/main/java/LibraryProject/LibraryProject/Server.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 import org.mindrot.jbcrypt.BCrypt;
 
@@ -39,11 +40,78 @@ public class Server {
                         System.out.println("Authentication successful!");
                         return true;
                     } else {
-                        System.out.println("Authentication failed. Passwords do not match.");
+                        System.out.println("Wrong password!");
                         return false;
                     }
                 } else {
                     System.out.println("User not found.");
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("Error executing the query: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return false;
+    }
+    
+    public static boolean verifyEmailUse(Connection connection, String email) {
+        Scanner scanner = new Scanner(System.in);
+        
+    	String query = "SELECT * FROM users WHERE email = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, email);
+
+            // Execute the query
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                	System.out.print("E-mail already in use!\nTry a different email: ");
+                	return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.err.println("Error executing the query: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return true;
+    }
+    
+    public static void insertUser(Connection connection, String email, String name, String surname, String password) {
+
+        String query = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        	preparedStatement.setInt(1, 0);
+        	preparedStatement.setString(2, email);
+            preparedStatement.setString(3, name);
+            preparedStatement.setString(4, surname);
+            preparedStatement.setString(5, BCrypt.hashpw(password, BCrypt.gensalt(12)));
+            preparedStatement.setInt(6, 0);
+
+            int affectedRows = preparedStatement.executeUpdate();
+            System.out.println("\nAccount created successfully!");
+        }
+        catch (SQLException e) {
+            System.err.println("Error executing the query: " + e.getMessage());
+            e.printStackTrace();
+        }
+    } 
+    
+    public static boolean checkAdmin(Connection connection, int user_id) {
+        String query = "SELECT * FROM users WHERE id_user = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, user_id);
+
+            // Execute the query
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                	if(resultSet.getBoolean("spravca")) {
+                    	return true;
+                    }else return false;
+                } else {
+                    return false;
                 }
             }
         }
--- src/main/java/LibraryProject/LibraryProject/main.java
+++ src/main/java/LibraryProject/LibraryProject/main.java
@@ -1,9 +1,7 @@
 package LibraryProject.LibraryProject;
 
 import org.mindrot.jbcrypt.BCrypt;
-
 import com.mysql.cj.protocol.Resultset;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -16,37 +14,43 @@ public class main {
 
     public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
-        int user_id;
+        int id_user;
 
         Connection connection = Server.establishConnection("jdbc:mysql://localhost:3306/mydb", "root", "");
         
-        user_id = menu(connection);
-        System.out.println(user_id);
-    	String query = "SELECT * FROM users WHERE id_user = ?";
+        id_user = menu1(connection);
+        boolean is_admin = Server.checkAdmin(connection, id_user);
+        System.out.println(is_admin);
+        
+        /*String query = "SELECT * FROM users WHERE id_user = ?";
     	resultset 
     	premenna = Resultset.getboolean("spravca");
-    	if()
+    	if()*/
 
     }
-    
-    
-//hsbahvashbvhiasbvihabvia
-    
+
     // *************** MENU - LOG IN / SIGN IN ***************
-    public static int menu(Connection connection) throws SQLException {
+    public static int menu1(Connection connection) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Welcome to the User Authentication System!");
-            System.out.println("1. Log In");
-            System.out.println("2. Create Account");
-            System.out.println("3. Exit");
+            System.out.println("\n**********************************************\n| Welcome to the User Authentication System! |");
+            System.out.println("|                                            |");
+            System.out.println("|               1. Log In                    |");
+            System.out.println("|           2. Create Account                |");
+            System.out.println("|                3. Exit                     |");
+            System.out.println("**********************************************");
 
-            System.out.print("Choose an option (1/2/3): ");
+            System.out.print("\nChoose an option (1/2/3): ");
             String choice = scanner.next();
 
             switch (choice) {
                 case "1":
-                    return logIn(connection);
+                    int user_id = logIn(connection);
+                    if (user_id == -1) {
+                    	break;
+                    } else {
+                    	return user_id;
+                    }
                 case "2":
                     createAccount(connection);
                     break;
@@ -64,9 +68,12 @@ public class main {
         System.out.println("Log In");
         System.out.print("Enter your email: ");
         String email = scanner.next();
+        while(!email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")) {
+            System.out.print("Wrong format try again: ");
+        	email = scanner.next();
+    	}
         System.out.print("Enter your password: ");
         String password = scanner.next();
-        
         boolean logged = Server.verifyLogIn(connection, email, password);
         if(logged) {
         	String query = "SELECT * FROM users WHERE email = ?";
@@ -88,13 +95,13 @@ public class main {
 	            e.printStackTrace();
 	        }
         }
-		return 0;
+		return -1;
         
     }
     
     private static void createAccount(Connection connection) {
         Scanner scanner = new Scanner(System.in);
-        boolean flag = true;
+        boolean flag = true, fFlag = true;
         
         System.out.println("Create Account");
         System.out.print("Enter your name: ");
@@ -103,34 +110,19 @@ public class main {
         String surname = scanner.next();
         System.out.print("Enter your email: ");
         String email = scanner.next();
-	    while(flag) {
+        while(flag) {
+        	if(fFlag) {
+        		fFlag = false;
+        	}else {
+            	email = scanner.next();	
+        	}
 	        while(!email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$")) {
 	            System.out.print("Wrong format try again: ");
 	        	email = scanner.next();
 	    	}
-
-	        String query = "SELECT * FROM users WHERE email = ?";
-	        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-	            preparedStatement.setString(1, email);
-
-	            // Execute the query
-	            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-	                if (resultSet.next()) {
-	                	System.out.print("E-mail already in use!\nTry again: ");
-	                	email = scanner.next();
-	                } else {
-	                    System.out.println("funguje.");
-	                    flag = false;
-	                }
-	            }
-	        }
-	        catch (SQLException e) {
-	            System.err.println("Error executing the query: " + e.getMessage());
-	            e.printStackTrace();
-	        }
-	        
-	    }
-	    System.out.print("Enter your password: ");
+	        flag = Server.verifyEmailUse(connection, email);
+        }
+        System.out.print("Enter your password: ");
         String password = "a", confirmPassword = "b";
         while(!password.equals(confirmPassword)) {
 	        password = scanner.next();
@@ -146,36 +138,7 @@ public class main {
         	}
 	    }
 	        
-
-        String query = "INSERT INTO users VALUES (?, ?, ?, ?, ?)";
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-        	preparedStatement.setInt(1, 0);
-        	preparedStatement.setString(2, email);
-            preparedStatement.setString(3, name);
-            preparedStatement.setString(4, surname);
-            preparedStatement.setString(5, BCrypt.hashpw(password, BCrypt.gensalt(12)));
-
-            int affectedRows = preparedStatement.executeUpdate();
-
-            if (affectedRows > 0) {
-                System.out.println("User inserted successfully!");
-            } else {
-                System.out.println("Failed to insert user.");
-            }
-        }
-        catch (SQLException e) {
-            System.err.println("Error executing the query: " + e.getMessage());
-            e.printStackTrace();
-        }
-        
-        
-        
-        // Perform account creation logic here
-        if (password.equals(confirmPassword)) {
-            System.out.println("Account created successfully!");
-        } else {
-            System.out.println("Passwords do not match. Account creation failed.");
-        }
+        Server.insertUser(connection, email, name, surname, password);
     }
 
 
